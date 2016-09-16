104a12657690cfbe2c0f294c814ac40e5f4fd85d
Meryem Tahar <mtahar@hotmail.co.uk>
2016-09-16 22:26:08 +0100

    first commit

---
+++ Java/robocode/eclipse/eclipse.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=c:\robocode\jdk8u77x64
+set PATH=%JAVA_HOME%\bin;%PATH%
+start eclipse.exe
---
+++ Python/HelloWorld.py
@@ -0,0 +1,6 @@
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+
+print helloworld
+print "Hello, Code Colliders!"
